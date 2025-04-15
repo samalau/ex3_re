c891adb967d7755b81ecac5884102e88bb5fd0e7
Sam Newmark <smanthanewmark@live.biu.ac.il>
2025-04-15 21:17:42 +0300

    ex3

--- ex3.c
+++ ex3.c
@@ -11,6 +11,7 @@ Assignment: ex3
 #define NUM_OF_TYPES 4
 #define TYPES_NAMES 10
 #define DAYS_IN_YEAR 365
+#define NONE -1
 #define UNSELECTED 0
 #define addOne  1
 #define addAll  2  
@@ -20,53 +21,59 @@ Assignment: ex3
 #define deltas  6
 #define done  7
 
-char brands[NUM_OF_BRANDS][BRANDS_NAMES] = {"Toyoga", "HyunNight", "Mazduh", "FolksVegan", "Key-Yuh"};
-char types[NUM_OF_TYPES][TYPES_NAMES] = {"SUV", "Sedan", "Coupe", "GT"};
+// most ind
+#define AMOUNT 0
+#define BRAND 1
+#define mSIZE 2
+// most arrs
+int mostSales[DAYS_IN_YEAR][mSIZE];
+int mostType[DAYS_IN_YEAR][mSIZE];
+
+char brands[NUM_OF_BRANDS][BRANDS_NAMES]={"Toyoga", "HyunNight", "Mazduh", "FolksVegan", "Key-Yuh"};
+char types[NUM_OF_TYPES][TYPES_NAMES]={"SUV", "Sedan", "Coupe", "GT"};
 int days[NUM_OF_BRANDS];
 int choice;
 
-long long enterNumber(int task) {
-	switch(task) {
-		case UNSELECTED: {
+long long enterNumber(int task){
+	switch(task){
+		case UNSELECTED:{
 			printMenu();
 			break;
 		}
-		case addOne: {
+		case addOne:{
 			printf("What brand?\n");
 			break;
 		}
-		default: {
+		default:{
 			break;
 		}
 	}
-	long long number = 0, temp = 0;
-	int input = 0;
-	while ((input = scanf(" %lld", &temp)) != 1 || temp < 0 || task == UNSELECTED) {
-		if (input == EOF) {
+	long long number=0, temp=0;
+	int input=0;
+	while((input=scanf(" %lld", &temp))!=1 || temp<0 || task==UNSELECTED){
+		if(input==EOF){
 			return EOF;
 		}
 		scanf("%*[^\n]");
-		switch(task) {
-			case UNSELECTED: {
-				if (temp == done) {
+		switch(task){
+			case UNSELECTED:{
+				if(temp==done){
 					return done;
 				}
-				if (temp >= addOne && temp <= done) {
-					number = temp;
+				if(temp>=addOne && temp <= done){
+					number=temp;
 					return number;
 				}
 				break;
 			}
-			default: {
+			default:{
 				break;
 			}
 		}
 	}
 	scanf("%*[^\n]");
-	number = temp;
-	return (task == UNSELECTED && number == done)
-					? terminate()
-				: number;
+	number=temp;
+	return number;
 }
 
 void printMenu(){
@@ -75,54 +82,148 @@ void printMenu(){
            "2.Populate A Day Of Sales For All Brands\n"
            "3.Provide Daily Stats\n"
            "4.Print All Data\n"
-           "5.Provide Overall (simple) Insights\n"
+           "5.Provide Overall(simple) Insights\n"
            "6.Provide Average Delta Metrics\n"
            "7.exit\n");
 }
 
-void initCube(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]) {
-	for (int i = 0; i < DAYS_IN_YEAR; i++) {
-		for (int j = 0; j < NUM_OF_BRANDS; j++) {
-			for (int k = 0; k < NUM_OF_TYPES; k++) {
-				cube[i][j][k] = -1;
+void displayDay(int brand){
+	int day=NONE;
+	while(++day<=days[brand]){
+		printf("Day %d-", day+1);
+		for(int i=0; i<DAYS_IN_YEAR; i++){
+			for(int j=0; j<NUM_OF_BRANDS; j++){
+				for(int k=0; k<NUM_OF_TYPES; k++){
+					printf(" %s: ", types[i]);
+				}
 			}
 		}
 	}
 }
 
-int chosenAddOne(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]) {
-    int brandIndex = enterNumber(addOne);
-    if (brandIndex == EOF) {
+int getDay(int latestDay){
+	int day = NONE;
+	printf("What day would you like to analyze?\n");
+	day = enterNumber(stats);
+	if (day<0 || day>latestDay){
+		printf("Please enter a valid day.\n");
+		getDay(latestDay);
+	}
+	return day;
+}
+
+void express(int focus){
+	switch(focus){
+		case addOne:{
+			printf("");
+			return;
+		}
+		case addAll:{
+			printf("");
+			return;
+		}
+		case stats:{
+			int day=NONE, latestDay=NONE, totalSum=NONE;
+			for(int i; i<NUM_OF_BRANDS; i++){
+				if(days[i]>latestDay){
+					latestDay=days[i];
+				}
+			}
+			if (latestDay==NONE){
+				return;
+			}
+			day=getDay(latestDay);
+			printf("In day number %d:\n", day);
+			// conditional
+			printf("The sales total was %d\n", totalSum);
+			printf("The best sold brand with %d sales was %s\n", mostSales[day][AMOUNT], mostSales[day][BRAND]);
+			printf("The best sold type with 9 sales was GT\n", mostType[day][AMOUNT], mostType[day][BRAND]);
+			return;
+		}
+		case print:{
+			int brand=NONE;
+			while(brand<NUM_OF_BRANDS){
+				printf("Sales for %s:\n", brands[brand]);
+				if(days[brand]==NONE){
+					continue;
+				}
+				displayDay(brand);
+			}
+			return;
+		}
+		case insights:{
+			int bBrand=bestBrand();
+			printf("The best-selling brand overall is %s: %d\n$", bBrand, sales(bBrand));
+			int bType=bestType();
+			printf("The best-selling type of car is %s: %d$\n", bType, sales(bType));
+			int bDay=bestDay();
+			printf("The most profitable day was day number %d: %d$\n", bDay, sales(bDay));
+			return;
+		}
+		case deltas:{
+			int brand=0;
+			printf("Brand: %s, Average Delta: %1.6f\n", brands[brand], avgDelta(brand++));
+			brand++;
+			return;
+		}
+		case done:{
+			printf("Goodbye!\n");
+			return;
+		}
+		default:{
+			return;
+		}
+	}
+}
+
+void init(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]){
+	for(int i=0; i<DAYS_IN_YEAR; i++){
+		mostSales[i][AMOUNT]=mostSales[i][BRAND]=NONE;
+		mostType[i][AMOUNT]=mostType[i][BRAND]=NONE;
+		for(int j=0; j<NUM_OF_BRANDS; j++){
+			days[j]=NONE;
+			for(int k=0; k<NUM_OF_TYPES; k++){
+				cube[i][j][k]=NONE;
+			}
+		}
+	}
+}
+
+int chosenAddOne(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]){
+    int brandIndex=enterNumber(addOne);
+    // program termination
+    if(brandIndex==EOF){
         return EOF;
     }
-    if (brandIndex < 0 || brandIndex >= NUM_OF_BRANDS) {
+    // invalid brand
+    if(brandIndex<0 || brandIndex>=NUM_OF_BRANDS){
         printf("This brand is not valid\n");
 		return UNSELECTED;
     }
-
+    // make function
     return 1;
 }
 
-int main() {
+int main(){
     int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
-    initCube(cube);
-    while(choice != done){
-        if ((choice = enterNumber(UNSELECTED)) == EOF || choice == done) {
+    init(cube);
+    while(choice!=done){
+        if((choice=enterNumber(UNSELECTED))==EOF || choice==done){
             break;
         }
-        if (choice < addOne || choice > deltas) {
-            choice = UNSELECTED;
+        if(choice<addOne || choice>deltas){
+            choice=UNSELECTED;
             scanf("%*[^\n]");
         }
         switch(choice){
-            case addOne: {
+            case addOne:{
                 chosenAddOne(cube);
                 break;
             }
-            case addAll: {
+            case addAll:{
                 break;
             }
-            default: {
+            default:{
                 printf("Invalid input\n");
             }
         }
@@ -130,5 +231,4 @@ int main() {
     printf("Goodbye!\n");
     return 0;
 }
-
 
