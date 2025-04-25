9260e5543f3828c22ebf7b003ba83bcdaf88b28c
Sam Newmark <smanthanewmark@live.biu.ac.il>
2025-04-25 19:08:55 +0300

    ex3

--- ex3.c
+++ ex3.c
@@ -6,98 +6,120 @@ Assignment: ex3
 
 #include <stdio.h>
 
+// no data
+#define NONE -1
 
-/* MACROS */				 			 /* GENERAL */
-		#define                    NONE          -1 		// no data
-		#define         UNSELECTED 	         0		   // menu unselected
+// menu unselected
+#define UNSELECTED 0
 
-							 					/* MENU OPTIONS */
-		#define	             	 addOne           1  	    	  // task/menu choice 1: single day entry
-		#define		               addAll           2              // task/menu choice 2: full day entry
-		#define		                 stats           3              // task/menu choice 3: daily stats
-		#define		                  print 	     4 	            // task/menu choice 4: print data
-		#define	                 insights	  	   5		      // task/menu choice 5: overall insights
-		#define	   		  	  	    deltas 		    6  	           // task/menu choice 6: average deltas
-		#define		    			  done          7  		       // menu choice 7: exit
+// task/menu selection 1: single day entry
+#define addOne 1
 
-						    				/* MEASUREMENTS */
-		#define 	   DAYS_IN_YEAR      365		// index: 0 to 364
-		#define   NUM_OF_BRANDS          5		  // number of car brands
-		#define		 BRANDS_NAMES         15	   // max brand name length
-		#define      NUM_OF_TYPES          4	    // number of car types
-		#define         TYPES_NAMES         10		 // max type name length
+// task/menu selection 2: full day entry
+#define addAll 2
 
+// task/menu selection 3: daily stats
+#define stats 3
 
-/* GLOBAL INTEGER VARIABLES */
-	int menuChoice;						// current main menu selection
-	int earliestDay = NONE;			// earliest entered day overall
-	int	latestDay = NONE;		     // latest entered day overall	
+// task/menu selection 4: print data
+#define	print 4
 
+// task/menu selection 5: overall insights
+#define insights 5
 
-/* GLOBAL INTEGER ARRAYS */
-		// latest day per brand
-			int	days[NUM_OF_BRANDS];
-		
-		// sales data cube
-			int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
+// task/menu selection 6: average deltas
+#define	deltas 6
 
+// menu selection 7: exit
+#define	done 7
 
-/* GLOBAL CHARACTER ARRAYS */
+// index: 0 to 364
+#define DAYS_IN_YEAR 365
 
-	// brand names
-		char brands[NUM_OF_BRANDS][BRANDS_NAMES]={		   
-			"Toyoga",
-			"HyunNight",
-			"Mazduh",
-			"FolksVegan",
-			"Key-Yuh"
-		};
+// number of car brands
+#define NUM_OF_BRANDS 5
 
-	// type names
-		char types [NUM_OF_TYPES][TYPES_NAMES]={
-			"SUV",
-			"Sedan",
-			"Coupe",
-			"GT"
-		}; 		
+// max brand name length
+#define BRANDS_NAMES 15
+
+// number of car types
+#define NUM_OF_TYPES 4
+
+// max type name length
+#define TYPES_NAMES 10
 
 
-/*FUNCTION PROTOTYPES*/
+// current main menu selection
+int menuSelection;
 
-	// initialize data cube cells to -1
-		void initCube();
-	// display main menu options
-		void printMenu();
-	// select option from main menu, update menuChoice global
-		void inputChoice();
-	// update (overall) earliest day global with valid data
-		void getEarliestDay();
-	// update (overall) latest day global with valid data
-		void getLatestDay();
-	// task 2: user selected to complete data for earliest incomplete day
-		int addAllChosen();
-	// get sales data per type per brand of earliest incomplete day
-		int inputData();
-	// identify what data for current day remains incomplete during input
-		int noticeNoData();
-	// task 3: user selected to display stats of chosen day
-		void statsChosen(int day);
-	// choose day to display stats
-		int inputDay();
-	// task 4: user selected to display all data
-		void printChosen();
-	// display all data
-		void displayData(int brand);
-	// get total sales of chosen day
-		int salesTotal(int day);
-	// get day with highest total sales 
-		void mostProfitableDay();
-	// get highest value of brand or type
-		void getBest(int path, int day, int subject, int items);
-	// task 5: user selected to display overall stats
-		void insightsChosen();
-	// task 6: user selected to display average delta of each brand
-		void deltasChosen();
+// earliest entered day overall
+int earliestDay = NONE;
+
+// latest entered day overall
+int	latestDay = NONE;
+
+
+// latest day per brand
+int	days[NUM_OF_BRANDS];
+	
+// sales data cube
+int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
+
+// names
+char brands[NUM_OF_BRANDS][BRANDS_NAMES]={"Toyoga", "HyunNight", "Mazduh", "FolksVegan", "Key-Yuh"};
+char types [NUM_OF_TYPES][TYPES_NAMES]={"SUV", "Sedan", "Coupe", "GT"}; 		
+
+
+// initialize data cube cells to -1
+void initCube();
+
+// display main menu options
+void printMenu();
+
+// select option from main menu, update menuSelection global
+void inputSelection();
+
+// update (overall) earliest day global with valid data
+void getEarliestDay();
+
+// update (overall) latest day global with valid data
+void getLatestDay();
+
+// task 2: user selected to complete data for earliest incomplete day
+int addAllChosen();
+
+// get sales data per type per brand of earliest incomplete day
+int inputData();
+
+// identify what data for current day remains incomplete during input
+int noticeNoData();
+
+// task 3: user selected to display stats of chosen day
+void statsChosen(int day);
+
+// choose day to display stats
+int inputDay();
+
+// task 4: user selected to display all data
+void printChosen();
+
+// display all data
+void displayData(int brand);
+
+// get total sales of chosen day
+int salesTotal(int day);
+
+// get day with highest total sales 
+void mostProfitableDay();
+
+// get highest value of brand or type
+void getBest(int path, int day, int subject, int items);
+
+// task 5: user selected to display overall stats
+void insightsChosen();
+
+// task 6: user selected to display average delta of each brand
+void deltasChosen();
 
 
 /* INITIALIZE DATA CUBE */
@@ -114,6 +136,7 @@ void initCube(){
 	}
 }
 
+
 /* DISPLAY MAIN MENU */
 void printMenu(){
 	printf("Welcome to the Cars Data Cube! What would you like to do?\n"
@@ -126,50 +149,52 @@ void printMenu(){
 	"7.exit\n");
 }
 
+
 /* MAIN MENU SELECTION WITH VALIDATION */
-void inputChoice(){
-	menuChoice=UNSELECTED;
+void inputSelection(){
+	menuSelection=UNSELECTED;
 	int input=UNSELECTED, temp=UNSELECTED;
 	while(printMenu(), (input=scanf(" %d",&temp))!=1 ||temp<addOne ||temp>done){
 		if(input==EOF){
-			menuChoice=done;
+			menuSelection=done;
 			return;
 		}
 		scanf("%*[^\n]");
 		printf("Invalid input\n");
 	}
 	scanf("%*[^\n]");
-	menuChoice=temp;
+	menuSelection=temp;
 }
+
 
 /* MAIN */
 int main(){
 	initCube();
-	while(menuChoice!=done){
+	while(menuSelection!=done){
 		/* MAIN UPDATES */
-		inputChoice();
+		inputSelection();
 		getLatestDay();
 		getEarliestDay();
 
 		/* MAIN MENU */
-		switch(menuChoice){
+		switch(menuSelection){
 			case done: break;
 			case addOne: {
 				if(inputData()==EOF){
-					menuChoice=done;
+					menuSelection=done;
 				}
 			break;
 			}
 			case addAll: {
 				if(addAllChosen()==EOF){
-					menuChoice=done;
+					menuSelection=done;
 				}
 			break;
 			}
 			case stats: {
 				int day=NONE;
 				day=inputDay();
-				day==EOF ?menuChoice=done :statsChosen(day);
+				day==EOF ?menuSelection=done :statsChosen(day);
 			break;
 			}
 			case print: {
@@ -194,6 +219,7 @@ int main(){
 	return 0;
 }
 
+
 /* USE TO CHECK IF OVERALL EARLIEST DAY MISSING DATA*/
 void getEarliestDay(){
 	earliestDay=DAYS_IN_YEAR;
@@ -204,6 +230,7 @@ void getEarliestDay(){
 	}
 }
 
+
 /* USE TO CHECK IF OVERALL LATEST DAY IS EXISTING AND VALID */
 void getLatestDay(){
 	for(int brand=0; brand<NUM_OF_BRANDS; brand++){
@@ -213,26 +240,30 @@ void getLatestDay(){
 	}
 }
 
+
 /* INPUT DATA WITH VALIDATION */
 int inputData(){
 	int input=0, today=earliestDay+1, tempBrand=NONE, tempST[NUM_OF_TYPES];
-
 	for(int type=0; type<NUM_OF_TYPES; type++){
 		tempST[type]=NONE;
 	}
 
 	// (brand index) + ($ of each type)
 	int validInput=1+NUM_OF_TYPES;
-	while((input=scanf(" %d %d %d %d %d", &tempBrand, &tempST[0], &tempST[1], &tempST[2], &tempST[3])) !=validInput
-	||tempBrand<0 ||tempBrand>=NUM_OF_BRANDS ||tempST[0]<0 ||tempST[1]<0 ||tempST[2]<0 ||tempST[3]<0
-	||cube[today][tempBrand][0]!=NONE){
+
+	while(
+		tempBrand=tempST[0]=tempST[1]=tempST[2]=tempST[3]=NONE,
+		(input=scanf(" %d %d %d %d %d", &tempBrand, &tempST[0], &tempST[1], &tempST[2], &tempST[3])) !=validInput
+		||tempBrand<0 ||tempBrand>=NUM_OF_BRANDS
+		||tempST[0]<0 ||tempST[1]<0 ||tempST[2]<0 ||tempST[3]<0
+		||cube[today][tempBrand][0]!=NONE
+	){
 		if(input==EOF){
-			menuChoice=done;
+			menuSelection=done;
 			return EOF;
 		}
 		scanf("%*[^\n]");
 		printf("This brand is not valid\n");
-		tempBrand=tempST[0]=tempST[1]=tempST[2]=tempST[3]=NONE;
 	}
 	scanf("%*[^\n]");
 	int brand=tempBrand;
@@ -248,14 +279,18 @@ int inputData(){
 	return 1;
 }
 
+
 /* SELECT DAY TO VIEW STATS*/
 int inputDay(){
 	int day=NONE, temp=NONE, input=UNSELECTED, last=latestDay+1;
-	while(temp=NONE,
-	printf("What day would you like to analyze?\n"),
-	(input=scanf(" %d",&temp))!=1 ||temp<1 ||temp>last){
+	while(
+		temp=NONE,
+		printf("What day would you like to analyze?\n"),
+		(input=scanf(" %d",&temp))!=1
+		||temp<1 ||temp>last
+	){
 		if(input==EOF){
-			menuChoice=done;
+			menuSelection=done;
 			return EOF;
 		}
 		scanf("%*[^\n]");
@@ -269,12 +304,16 @@ int inputDay(){
 	return day;
 }
 
+
 /* DAY WITH PARTIAL DATA IDENTIFICATION AND NOTIFICATION */
 int noticeNoData(){
 	int today=earliestDay+1, foundMissing=0;
+
 	if(today<DAYS_IN_YEAR){
 		for(int brand=0; brand<NUM_OF_BRANDS; brand++){
+
 			if(cube[today][brand][0]==NONE){
+				
 				if(!foundMissing){
 					printf("No data for brands %s", brands[brand]);
 					foundMissing=1;
@@ -287,11 +326,14 @@ int noticeNoData(){
 	return (foundMissing)?printf("\nPlease complete the data\n") :0;
 }
 
+
 /* HELPER FOR TASK 3 & 5*/
 int salesTotal(int day){
 	int temp=0;
+
 	for(int i=0; i<NUM_OF_TYPES; i++){
 		for(int j=0; j<NUM_OF_BRANDS; j++){
+			
 			if(cube[day][j][i]>NONE){
 				temp+=cube[day][j][i];
 			}
@@ -300,9 +342,11 @@ int salesTotal(int day){
 	return temp;
 }
 
+
 /* HELPER FOR TASK 5 */
 void mostProfitableDay(){
 	int temp=0, best=0, sales=0;
+
 	for(int i=0; i<latestDay; i++){
 		if((temp=salesTotal(i))>sales){
 			sales=temp;
@@ -312,12 +356,15 @@ void mostProfitableDay(){
 	printf("The most profitable day was day number %d: %d$\n", best+1, sales);
 }
 
+
 /* HELPER FOR TASKS 3 & 5 */
 void getBest(int path, int day, int subject, int items){
 	int isStats=(path==stats), isBrand=(subject==NUM_OF_BRANDS), temp=0, best=0, sales=0, x=0, y=0;
 	int start=isStats?day:0;
+
 	for(int a=0; a<subject; temp=0, a++){
 		for(int numDay=start; numDay<=day; numDay++){
+
 			for(int b=0; isBrand?(x=a, y=b):(y=a, x=b), b<items; b++){
 				if(cube[numDay][x][y]>NONE){
 					temp+=cube[numDay][x][y];
@@ -326,12 +373,15 @@ void getBest(int path, int day, int subject, int items){
 			if(isStats){
 				break;
 			}
+
 		}
 		if(temp>sales){
 			sales=temp;
 			best=a;
 		}
+	
 	}
+	
 	isStats ? isBrand
 		? printf("The best sold brand with %d sales was %s\n", sales, brands[best])
 		: printf("The best sold type with %d sales was %s\n", sales, types[best])
@@ -340,13 +390,14 @@ void getBest(int path, int day, int subject, int items){
 		: printf("The best-selling type of car is %s: %d$\n", types[best], sales);
 }
 
+
 /* TASK 2 CHECKPOINT */
 int addAllChosen(){
 	for(int brand=0; brand<NUM_OF_BRANDS; brand++){
 		if(days[brand]<DAYS_IN_YEAR-1){
 			while(noticeNoData()){
 				if(inputData()==EOF){
-					menuChoice=done;
+					menuSelection=done;
 					return EOF;
 				}
 			}
@@ -354,6 +405,7 @@ int addAllChosen(){
 	}
 	return 1;
 }
+
 
 /* TASK 3 CHECKPOINT */
 void statsChosen(int day){
@@ -368,6 +420,7 @@ void statsChosen(int day){
 	getBest(stats, day, NUM_OF_TYPES, NUM_OF_BRANDS);
 }
 
+
 /* TASK 4 HELPER */
 void displayData(int brand){
 	for(int day=0; day<=days[brand]; day++){
@@ -378,6 +431,7 @@ void displayData(int brand){
 		printf("\n");
 	}
 }
+
 
 /* TASK 4 CHECKPOINT */
 void printChosen(){
@@ -393,12 +447,14 @@ void printChosen(){
 	printf("*****************************************\n");
 }
 
+
 /* TASK 5 CHECKPOINT */
 void insightsChosen(){
 	getBest(insights, latestDay, NUM_OF_BRANDS, NUM_OF_TYPES);
 	getBest(insights, latestDay, NUM_OF_TYPES, NUM_OF_BRANDS);
 	mostProfitableDay();
 }
+
 
 /* TASK 6 CHECKPOINT */
 void deltasChosen(){
